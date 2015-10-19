271e0c34fc55afdee09b6bfce39108073c1691d4
Tony Kirke <tony@pyspuc.com>
2015-10-19 08:33:43 -0700

    Redo formula for chebyshev poles to fix odd orders

--- spuce/filters/chebyshev_iir.cpp
+++ spuce/filters/chebyshev_iir.cpp
@@ -20,24 +20,22 @@ void chebyshev_iir(iir_coeff& filt, float_type fcd, float_type ripple = 3.0) {
 }
 //! Calculate poles (chebyshev)
 void chebyshev_s(iir_coeff& filt, float_type wp, float_type epi, long n, long n2) {
-  auto l = 0;
-  if (n % 2 == 0) l = 1;
+  auto l = 1;
   float_type arg;
   float_type x = 1 / epi;
-  float_type asinh = log(x + sqrt(1.0 + x * x));
-  float_type v0 = asinh / (float_type(n));
-  float_type sm = sinh(v0);
-  float_type cm = cosh(v0);
+  float_type lambda = pow(x*(1.0 + sqrt(1.0 + epi*epi)),1.0/n);
+  float_type sm = 0.5*((1.0/lambda) - lambda);
+  float_type cm = 0.5*((1.0/lambda) + lambda);
   for (int j = 0; j < n2; j++) {
-    arg = -0.5 * M_PI * l / ((float_type)(n));
+    arg = 0.5 * M_PI * (2*l-1) / ((float_type)(n));
     if (filt.get_type()) {
-      filt.set_pole(wp * std::complex<float_type>(-sm * sin(arg), cm * cos(arg)), j);
-      filt.set_zero(FLT_MAX, j);
+			filt.set_pole(wp * std::complex<float_type>(sm * sin(arg), cm * cos(arg)), n2-1-j);
+      filt.set_zero(FLT_MAX, n2-1-j);
     } else {
-      filt.set_pole(1.0 / (wp * std::complex<float_type>(-sm * sin(arg), cm * cos(arg))), j);
-      filt.set_zero(0, j);
+      filt.set_pole(1.0 / (wp * std::complex<float_type>(sm * sin(arg), cm * cos(arg))), n2-1-j);
+      filt.set_zero(0, n2-1-j);
     }
-    l += 2;
+    l++;
   }
 }
 }  // namespace spuce
