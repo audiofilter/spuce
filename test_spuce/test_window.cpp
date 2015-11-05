5bd7b97e1c3747a811f3a8c42d6cc31185740d5d
Tony Kirke <tony@pyspuc.com>
2015-11-04 20:50:38 -0800

    To check window functions

---
+++ test_spuce/test_window.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <spuce/filters/design_window.h>
+using namespace std;
+using namespace spuce;
+int main(int argv, char* argc[]) {
+    int order = 8;
+
+    // hanning,hann,hamming,blackman,bartlett,kaiser,
+    std::string win_type = "chebyshev";
+    float_type beta = 8;
+    
+    std::vector<float_type> taps = design_window(win_type, order, beta);
+    std::cout << "Taps[] = {";
+    for (size_t i=0;i<taps.size();i++) std::cout << taps[i] << " ";
+    std::cout << "}\n";
+    taps = design_window(win_type, order+1, beta);
+    std::cout << "Taps[] = {";
+    for (size_t i=0;i<taps.size();i++) std::cout << taps[i] << " ";
+    std::cout << "}\n";
+
+  return(1);
+}
