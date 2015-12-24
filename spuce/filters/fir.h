81844091c3cf9d3102d3e592fac593172cbe1ad2
Tony Kirke <tony@pyspuc.com>
2015-12-23 20:10:12 -0800

    more type changes for windows

--- spuce/filters/fir.h
+++ spuce/filters/fir.h
@@ -20,29 +20,29 @@ template <class Numeric, class Coeff = float_type> class fir {
   //! Set tap weights
   void settap(size_t i, Coeff tap) { coeff[i] = tap; }
   void settaps(fir_coeff<Coeff>& c) {
-    for (int i = 0; i < num_taps; i++) coeff[i] = c.gettap(i);
+    for (size_t i = 0; i < num_taps; i++) coeff[i] = c.gettap(i);
   }
   size_t number_of_taps() const { return (num_taps); }
   Coeff gettap(size_t i) const { return (coeff[i]); }
   //! Reset
   void reset() {
-    for (int i = 0; i < num_taps; i++) z[i] = (Numeric)0;
+    for (size_t i = 0; i < num_taps; i++) z[i] = (Numeric)0;
     output = (Numeric)0;
   }
   //! Get sum of coefficients
   Coeff coeff_sum() {
-    int i;
+    size_t i;
     Coeff s;
     for (s = 0, i = 0; i < num_taps; i++) s += coeff[i];
     return (s);
   }
   void normalize_gain() {
     float_type c = 1.0 / coeff_sum();
-    for (int i = 0; i < num_taps; i++) coeff[i] *= c;
+    for (size_t i = 0; i < num_taps; i++) coeff[i] *= c;
   }
   void make_hpf() {
     bool inv = true;
-    for (int i = 0; i < num_taps; i++) {
+    for (size_t i = 0; i < num_taps; i++) {
       if (inv) coeff[i] *= -1;
       inv = !inv;
     }
@@ -58,7 +58,7 @@ template <class Numeric, class Coeff = float_type> class fir {
   //! Constructor
   fir(size_t n) : coeff(n), z(n), num_taps(n) {
 		assert(n > 0);
-    int i;
+    size_t i;
     if (n > 0) {
       for (i = 0; i < n; i++) z[i] = (Numeric)0;
       for (i = 0; i < n; i++) coeff[i] = (Coeff)0;
@@ -66,8 +66,8 @@ template <class Numeric, class Coeff = float_type> class fir {
   }
   //! Constructor
   fir(fir_coeff<Coeff> C) : coeff(C.number_of_taps()), z(C.number_of_taps()) {
-    int i;
-    int n = num_taps = C.number_of_taps();
+    size_t i;
+    size_t n = num_taps = C.number_of_taps();
     if (n > 0) {
       coeff.resize(n);
       z.resize(n);
@@ -76,8 +76,8 @@ template <class Numeric, class Coeff = float_type> class fir {
     }
   }
   fir(const std::vector<Coeff>& C) : coeff(C.size()), z(C.size()) {
-    int i;
-    int n = num_taps = C.size();
+    size_t i;
+    size_t n = num_taps = C.size();
     if (n > 0) {
       coeff.resize(n);
       z.resize(n);
@@ -86,8 +86,8 @@ template <class Numeric, class Coeff = float_type> class fir {
     }
   }
   void set_coeffs(fir_coeff<Coeff> C) {
-    int i;
-    int n = num_taps = C.number_of_taps();
+    size_t i;
+    size_t n = num_taps = C.number_of_taps();
     if (n > 0) {
       coeff.resize(n);
       z.resize(n);
@@ -104,8 +104,8 @@ template <class Numeric, class Coeff = float_type> class fir {
 			if (n > 0) {
 				coeff.resize(n);
 				z.resize(n);
-				for (int i = 0; i < n; i++) z[i] = (Numeric)0;
-				for (int i = 0; i < n; i++) coeff[i] = (Coeff)0;
+				for (size_t i = 0; i < n; i++) z[i] = (Numeric)0;
+				for (size_t i = 0; i < n; i++) coeff[i] = (Coeff)0;
 			} else {
 				coeff.resize(0);
 				z.resize(0);
@@ -120,46 +120,44 @@ template <class Numeric, class Coeff = float_type> class fir {
   //! Update filter by inputting 1 sample and returning convolved output sample.
   Numeric clock(Numeric in) { return (update(in)); }
   Numeric update(Numeric in) {
-    int i;
     sum_type sum(0);
     // Update history of inputs
-    for (i = num_taps - 1; i > 0; i--) z[i] = z[i - 1];
+    for (size_t i = num_taps - 1; i > 0; i--) z[i] = z[i - 1];
     // Add new input
     z[0] = in;
     // Perform FIR
-    for (i = 0; i < num_taps; i++) sum = sum + coeff[i] * z[i];
-
+    for (size_t i = 0; i < num_taps; i++) sum = sum + coeff[i] * z[i];
     output = (sum);
     return (output);
   }
   void process(const std::vector<Numeric>& in, std::vector<Numeric>& out) {
     // Update history of inputs
-    for (int j = 0; j < in.size(); j++) {
-      for (int i = num_taps - 1; i > 0; i--) z[i] = z[i - 1];
+    for (size_t j = 0; j < in.size(); j++) {
+      for (size_t i = num_taps - 1; i > 0; i--) z[i] = z[i - 1];
       // Add new input
       z[0] = in[j];
       // Perform FIR
       sum_type sum(0);
-      for (int i = 0; i < num_taps; i++) sum = sum + coeff[i] * z[i];
+      for (size_t i = 0; i < num_taps; i++) sum = sum + coeff[i] * z[i];
       out[j] = (sum);
     }
   }
   void process_inplace(std::vector<Numeric>& io) {
     // Update history of inputs
-    for (int j = 0; j < io.size(); j++) {
-      for (int i = num_taps - 1; i > 0; i--) z[i] = z[i - 1];
+    for (size_t j = 0; j < io.size(); j++) {
+      for (size_t i = num_taps - 1; i > 0; i--) z[i] = z[i - 1];
       // Add new input
       z[0] = io[j];
       // Perform FIR
       sum_type sum(0);
-      for (int i = 0; i < num_taps; i++) sum = sum + coeff[i] * z[i];
+      for (size_t i = 0; i < num_taps; i++) sum = sum + coeff[i] * z[i];
       io[j] = (sum);
     }
   }
   // Tapped delay line uses previous outputs (to behave like an IIR)
   Numeric iir(Numeric in) {
     typename mixed_type<Numeric, Coeff>::dtype sum;
-    int i;
+    size_t i;
     for (sum = 0, i = 0; i < num_taps; i++) sum = sum + coeff[i] * z[i];
     // Update history of outputs
     for (i = num_taps - 1; i > 0; i--) z[i] = z[i - 1];
@@ -169,15 +167,15 @@ template <class Numeric, class Coeff = float_type> class fir {
     z[0] = output;
     return (output);
   }
-  int read_taps(const char* file) {
+  size_t read_taps(const char* file) {
     fir_coeff<Coeff> c(num_taps);
-    int r = c.read_taps(file);
-    for (int i = 0; i < num_taps; i++) coeff[i] = c.coeff[i];
+    size_t r = c.read_taps(file);
+    for (size_t i = 0; i < num_taps; i++) coeff[i] = c.coeff[i];
     return (r);
   }
   void print(void) {
     fir_coeff<Coeff> c(num_taps);
-    for (int i = 0; i < num_taps; i++) c.settap(i, coeff[i]);
+    for (size_t i = 0; i < num_taps; i++) c.settap(i, coeff[i]);
     c.print();
   }
 
@@ -185,9 +183,9 @@ template <class Numeric, class Coeff = float_type> class fir {
   template <class N, class C> friend std::vector<N> get_input(const fir<N, C>& y);
   void set_taps(const std::vector<Coeff>& taps) {
     set_size(taps.size()); // Make sure it's the right size first
-    for (int i = 0; i < num_taps; i++) coeff[i] = taps[i];
+    for (size_t i = 0; i < num_taps; i++) coeff[i] = taps[i];
   }
-	Coeff get_coeff(int i) const { return coeff[i];}
+	Coeff get_coeff(size_t i) const { return coeff[i];}
 	
  protected:
   std::vector<Coeff> coeff;
@@ -201,13 +199,13 @@ template <class Numeric, class Coeff = float_type> class fir {
 template <class Numeric, class Coeff> std::vector<Coeff> get_taps(const fir<Numeric, Coeff>& f) {
   size_t N = f.num_taps;
   std::vector<Coeff> V(N);
-  for (int i = 0; i < N; i++) V[i] = f.coeff[i];
+  for (size_t i = 0; i < N; i++) V[i] = f.coeff[i];
   return (V);
 }
 template <class Numeric, class Coeff> std::vector<Numeric> get_input(const fir<Numeric, Coeff>& f) {
   size_t N = f.num_taps;
   std::vector<Numeric> V(N);
-  for (int i = 0; i < N; i++) V[i] = f.z[i];
+  for (size_t i = 0; i < N; i++) V[i] = f.z[i];
   return (V);
 }
 }  // namespace spuce
